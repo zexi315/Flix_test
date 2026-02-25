6ecc795251ded484e18d0b765e7096e78e2b2f5d
zexi315 <yuejiaqi315@gmail.com>
2026-02-25 17:03:10 +0800

    time

---
+++ time.ino
@@ -0,0 +1,29 @@
+// Copyright (c) 2023 Oleg Kalachev <okalachev@gmail.com>
+// Repository: https://github.com/okalachev/flix
+// 时间相关函数
+// Time related functions
+
+float loopRate; // Hz
+
+void step() {
+	float now = micros() / 1000000.0;
+	dt = now - t;
+	t = now;
+
+	if (!(dt > 0)) {
+		dt = 0; // assume dt to be zero on first step and on reset
+	}
+
+	computeLoopRate();
+}
+
+void computeLoopRate() {
+	static float windowStart = 0;
+	static uint32_t rate = 0;
+	rate++;
+	if (t - windowStart >= 1) { // 1 second window
+		loopRate = rate;
+		windowStart = t;
+		rate = 0;
+	}
+}
