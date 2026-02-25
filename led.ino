912979b4771c575ca34c4f6403eeffb2a131685b
zexi315 <yuejiaqi315@gmail.com>
2026-02-25 16:59:33 +0800

    板载LED灯控制

---
+++ led.ino
@@ -0,0 +1,27 @@
+// Copyright (c) 2023 Oleg Kalachev <okalachev@gmail.com>
+// Repository: https://github.com/okalachev/flix
+// 板载LED灯控制
+// Board's LED control
+
+#define BLINK_PERIOD 500000
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2 // for ESP32 Dev Module
+#endif
+
+void setupLED() {
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void setLED(bool on) {
+	static bool state = false;
+	if (on == state) {
+		return; // don't call digitalWrite if the state is the same
+	}
+	digitalWrite(LED_BUILTIN, on ? HIGH : LOW);
+	state = on;
+}
+
+void blinkLED() {
+	setLED(micros() / BLINK_PERIOD % 2);
+}
