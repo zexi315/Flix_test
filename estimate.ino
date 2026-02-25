c259492cac1a56098c9cc5f978fd269971e7b3a5
zexi315 <yuejiaqi315@gmail.com>
2026-02-25 16:58:18 +0800

    姿态估算
    互补滤波

---
+++ estimate.ino
@@ -0,0 +1,40 @@
+// Copyright (c) 2023 Oleg Kalachev <okalachev@gmail.com>
+// Repository: https://github.com/okalachev/flix
+// 基于陀螺仪和加速度计的姿态计算
+// Attitude estimation from gyro and accelerometer
+
+#include "quaternion.h"
+#include "vector.h"
+#include "lpf.h"
+#include "util.h"
+
+float accWeight = 0.003;
+LowPassFilter<Vector> ratesFilter(0.2); // cutoff frequency ~ 40 Hz
+
+void estimate() {
+	applyGyro();
+	applyAcc();
+}
+
+void applyGyro() {
+	// filter gyro to get angular rates
+	rates = ratesFilter.update(gyro);
+
+	// apply rates to attitude
+	attitude = Quaternion::rotate(attitude, Quaternion::fromRotationVector(rates * dt));
+}
+
+void applyAcc() {
+	// test should we apply accelerometer gravity correction
+	float accNorm = acc.norm();
+	landed = !motorsActive() && abs(accNorm - ONE_G) < ONE_G * 0.1f;
+
+	if (!landed) return;
+
+	// calculate accelerometer correction
+	Vector up = Quaternion::rotateVector(Vector(0, 0, 1), attitude);
+	Vector correction = Vector::rotationVectorBetween(acc, up) * accWeight;
+
+	// apply correction
+	attitude = Quaternion::rotate(attitude, Quaternion::fromRotationVector(correction));
+}
