979881ac7076d941ed53a98c678ce209409cb2bf
zexi315 <yuejiaqi315@gmail.com>
2026-02-25 17:03:20 +0800

    util

---
+++ util.h
@@ -0,0 +1,85 @@
+// Copyright (c) 2023 Oleg Kalachev <okalachev@gmail.com>
+// Repository: https://github.com/okalachev/flix
+//实用函数
+// Utility functions
+
+#pragma once
+
+#include <math.h>
+#include <soc/soc.h>
+#include <soc/rtc_cntl_reg.h>
+
+const float ONE_G = 9.80665;
+extern float t;
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+bool invalid(float x) {
+	return !isfinite(x);
+}
+
+bool valid(float x) {
+	return isfinite(x);
+}
+
+// Wrap angle to [-PI, PI)
+float wrapAngle(float angle) {
+	angle = fmodf(angle, 2 * PI);
+	if (angle > PI) {
+		angle -= 2 * PI;
+	} else if (angle < -PI) {
+		angle += 2 * PI;
+	}
+	return angle;
+}
+
+// Disable reset on low voltage
+void disableBrownOut() {
+	REG_CLR_BIT(RTC_CNTL_BROWN_OUT_REG, RTC_CNTL_BROWN_OUT_ENA);
+}
+
+// Trim and split string by spaces
+void splitString(String& str, String& token0, String& token1, String& token2) {
+	str.trim();
+	char chars[str.length() + 1];
+	str.toCharArray(chars, str.length() + 1);
+	token0 = strtok(chars, " ");
+	token1 = strtok(NULL, " "); // String(NULL) creates empty string
+	token2 = strtok(NULL, "");
+}
+
+// Rate limiter
+class Rate {
+public:
+	float rate;
+	float last = 0;
+	Rate(float rate) : rate(rate) {}
+
+	operator bool() {
+		if (t - last >= 1 / rate) {
+			last = t;
+			return true;
+		}
+		return false;
+	}
+};
+
+// Delay filter for boolean signals - ensures the signal is on for at least 'delay' seconds
+class Delay {
+public:
+	float delay;
+	float start = NAN;
+	Delay(float delay) : delay(delay) {}
+
+	bool update(bool on) {
+		if (!on) {
+			start = NAN;
+			return false;
+		} else if (isnan(start)) {
+			start = t;
+		}
+		return t - start >= delay;
+	}
+};
