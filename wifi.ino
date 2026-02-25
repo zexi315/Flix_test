ad4ca5b9dfa9924081df8828803d215d8339ca6b
zexi315 <yuejiaqi315@gmail.com>
2026-02-25 17:04:08 +0800

    wifi

---
+++ wifi.ino
@@ -0,0 +1,49 @@
+// Copyright (c) 2023 Oleg Kalachev <okalachev@gmail.com>
+// Repository: https://github.com/okalachev/flix
+// Wi-Fi功能支持
+// Wi-Fi support
+
+#if WIFI_ENABLED
+
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiUdp.h>
+
+#define WIFI_SSID "Drone_WiFi"
+#define WIFI_PASSWORD "12345678"
+#define WIFI_UDP_PORT 14550
+#define WIFI_UDP_REMOTE_PORT 14550
+#define WIFI_UDP_REMOTE_ADDR "255.255.255.255"
+
+WiFiUDP udp;
+
+void setupWiFi() {
+	print("Setup Wi-Fi\n");
+	WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+	udp.begin(WIFI_UDP_PORT);
+}
+
+void sendWiFi(const uint8_t *buf, int len) {
+	if (WiFi.softAPIP() == IPAddress(0, 0, 0, 0) && WiFi.status() != WL_CONNECTED) return;
+	udp.beginPacket(udp.remoteIP() ? udp.remoteIP() : WIFI_UDP_REMOTE_ADDR, WIFI_UDP_REMOTE_PORT);
+	udp.write(buf, len);
+	udp.endPacket();
+}
+
+int receiveWiFi(uint8_t *buf, int len) {
+	udp.parsePacket();
+	return udp.read(buf, len);
+}
+
+void printWiFiInfo() {
+	print("MAC: %s\n", WiFi.softAPmacAddress().c_str());
+	print("SSID: %s\n", WiFi.softAPSSID().c_str());
+	print("Password: %s\n", WIFI_PASSWORD);
+	print("Clients: %d\n", WiFi.softAPgetStationNum());
+	print("Status: %d\n", WiFi.status());
+	print("IP: %s\n", WiFi.softAPIP().toString().c_str());
+	print("Remote IP: %s\n", udp.remoteIP().toString().c_str());
+	print("MAVLink connected: %d\n", mavlinkConnected);
+}
+
+#endif
